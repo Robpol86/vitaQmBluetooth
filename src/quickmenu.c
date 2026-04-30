1ce3e5c28d3d7b099be7314b0d79a30c1e75a8f9
Robpol86 <robpol86@gmail.com>
2026-04-30 20:12:57 +0900

    change label for now

--- src/quickmenu.c
+++ src/quickmenu.c
@@ -45,14 +45,14 @@ void quickmenu_start() {
     QuickMenuRebornSetWidgetSize(ID_SECTION_TEXT, SCE_PLANE_WIDTH, 50, 0, 0);
     QuickMenuRebornSetWidgetPosition(ID_SECTION_TEXT, 0, 25, 0, 0);
     QuickMenuRebornSetWidgetColor(ID_SECTION_TEXT, 1, 1, 1, 1);
-    QuickMenuRebornSetWidgetLabel(ID_SECTION_TEXT, "Bluetooth Devices");
+    QuickMenuRebornSetWidgetLabel(ID_SECTION_TEXT, "Accessibility RP");  // TODO Bluetooth Devices
 
     // Add placeholder "Loading" text.
     QuickMenuRebornRegisterWidget(ID_LOADING_TEXT, ID_PLANE_ROOT, text);
     QuickMenuRebornSetWidgetSize(ID_LOADING_TEXT, SCE_PLANE_WIDTH, 75, 0, 0);
     QuickMenuRebornSetWidgetPosition(ID_LOADING_TEXT, 0, -25, 0, 0);
     QuickMenuRebornSetWidgetColor(ID_LOADING_TEXT, 1, 1, 1, 1);
-    QuickMenuRebornSetWidgetLabel(ID_LOADING_TEXT, "Loading...");
+    QuickMenuRebornSetWidgetLabel(ID_LOADING_TEXT, "Invert Colors RP");  // TODO Loading...
 
     // TODO style: alignment and spacing. Then PR.
 }
