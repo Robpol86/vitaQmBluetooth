ce184cc171d96b7f67fc165082231310f4177029
Robpol86 <robpol86@gmail.com>
2026-05-04 16:28:29 +0900

    prune and todo

--- module_kernel/src/main.c
+++ module_kernel/src/main.c
@@ -17,19 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include <psp2/kernel/error.h>
-#include <psp2kern/ctrl.h>
-#include <psp2kern/io/fcntl.h>
-#include <psp2kern/io/stat.h>
-#include <psp2kern/kernel/cpu.h>
 #include <psp2kern/kernel/debug.h>
 #include <psp2kern/kernel/modulemgr.h>
-#include <psp2kern/kernel/sysmem.h>
-#include <psp2kern/kernel/threadmgr.h>
-#include <psp2kern/kernel/utils.h>
-#include <psp2kern/netps.h>
-#include <stdarg.h>
-#include <taihen.h>
 
 #include "log.h"
 
@@ -37,7 +26,7 @@ int module_start(SceSize argc, const void* args) {
     (void)argc;
     (void)args;
 
-    LOG_DEBUG("TODO RP");
+    LOG_DEBUG("TODO RP"); // TODO not working, maybe recv message from QuickMenu then emit, then respond, then QM emits ack.
 
     return SCE_KERNEL_START_SUCCESS;
 }
