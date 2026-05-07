de61a6b6de328d91434fbbcbee563da2e3190f4d
Robpol86 <robpol86@gmail.com>
2026-05-07 11:13:59 +0900

    AI example

--- module_kernel/src/bluetooth.c
+++ module_kernel/src/bluetooth.c
@@ -18,12 +18,43 @@ this program. If not, see <https://www.gnu.org/licenses/>.
  * TODO bluetooth interface.
  */
 
+#include <psp2kern/bt.h>
+#include <psp2kern/kernel/modulemgr.h>
+#include <psp2kern/kernel/sysmem.h>
+#include <psp2kern/kernel/threadmgr.h>
+
 #include "log.h"
+
+#define MAX_DEVICES 8  // TODO
 
 /**
  * TODO just log.
  */
 void log_paired_devices() {
-    // TODO.
-    LOG_DEBUG("TODO ENUMERATE AND LIST");
+    SceBtRegisteredInfo info;
+    int count = 0;
+
+    // First call: device=0, unk=0. Subsequent: walk via prev MAC.
+    // Stop on SCE_BT_ERROR_REG_DELETE_NO_ENTRY (0x802F0102) or any error.
+    unsigned int prev_mac_lo = 0;
+
+    for (int i = 0; i < MAX_DEVICES; i++) {
+        int ret = ksceBtGetRegisteredInfo(0, prev_mac_lo, &info, sizeof(info));
+        if (ret < 0) {
+            LOG_DEBUG("ksceBtGetRegisteredInfo[%d] returned 0x%08X (end or error)", i, ret);
+            break;
+        }
+
+        // The MAC in the struct is 6 bytes; print it and the name.
+        const unsigned char* m = (const unsigned char*)&info.mac;
+        LOG_DEBUG("Device %d: %02X:%02X:%02X:%02X:%02X:%02X  name=\"%s\"", i, m[0], m[1], m[2], m[3], m[4], m[5],
+                  info.name);
+
+        // Pack low word of MAC for next iteration.
+        // NOTE: This is the conventional pattern. Verify on hardware!
+        prev_mac_lo = (m[2] << 24) | (m[3] << 16) | (m[4] << 8) | m[5];
+        count++;
+    }
+
+    LOG_DEBUG("Enumerated %d paired device(s)", count);
 }
