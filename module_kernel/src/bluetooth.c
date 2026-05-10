71a3ca858de98cc974a43e3fa67f210840e0155e
Robpol86 <robpol86@gmail.com>
2026-05-10 12:08:33 +0900

    bt_cb_func draft1

--- module_kernel/src/bluetooth.c
+++ module_kernel/src/bluetooth.c
@@ -30,6 +30,104 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 #define MAX_DEVICES 8  // Maximum number of bluetooth devices the PS Vita can be paired with.
 
 /**
+ * TODO
+ */
+static int bt_cb_func(int notifyId, int notifyCount, int notifyArg, void* common) {
+    // static SceBtHidRequest hid_request;
+    static unsigned char recv_buff[0x100];
+
+    while (1) {
+        int ret;
+        SceBtEvent hid_event;
+
+        memset(&hid_event, 0, sizeof(hid_event));
+
+        do {
+            ret = ksceBtReadEvent(&hid_event, 1);
+        } while (ret == SCE_BT_ERROR_CB_OVERFLOW);
+
+        if (ret <= 0) {
+            break;
+        }
+
+        LOG_DEBUG("->Event:");
+        for (int i = 0; i < 0x10; i++) LOG_DEBUG(" %02X", hid_event.data[i]);
+
+        LOG_DEBUG("hid_event.mac0=%08X hid_event.mac1=%08X", hid_event.mac0, hid_event.mac1);
+
+        switch (hid_event.id) {
+            case 0x01: { /* Inquiry result event */
+                unsigned short vid_pid[2];
+                ksceBtGetVidPid(hid_event.mac0, hid_event.mac1, vid_pid);
+                LOG_DEBUG("inquiry vid_pid=%04X:%04X", vid_pid[0], vid_pid[1]);
+                break;
+            }
+
+            case 0x02: /* Inquiry stop event */
+                LOG_DEBUG("Inquiry stop event");
+                break;
+
+            case 0x04: /* Link key request? event */
+                LOG_DEBUG("link key request event");
+                ksceBtReplyUserConfirmation(hid_event.mac0, hid_event.mac1, 1);
+                break;
+
+            case 0x05: { /* Connection accepted event */
+                unsigned short vid_pid[2];
+                ksceBtGetVidPid(hid_event.mac0, hid_event.mac1, vid_pid);
+                LOG_DEBUG("connect accepted vid_pid=%04X:%04X", vid_pid[0], vid_pid[1]);
+                break;
+            }
+
+            case 0x06: /* Device disconnect event*/
+                LOG_DEBUG("device disconnect event");
+                break;
+
+            case 0x08: /* Connection requested event */
+                /*
+                 * Do nothing since we will get a 0x05 event afterwards.
+                 */
+                LOG_DEBUG("connection requested event");
+                break;
+
+            case 0x09: /* Connection request without being paired? event */
+                /*
+                 * The Vita needs to have a pairing with the DS4,
+                 * otherwise it won't connect.
+                 */
+                LOG_DEBUG("connection request without being paired event");
+                break;
+
+            case 0x0A: /* HID reply to 0-type request */
+
+                LOG_DEBUG("DS4 0x0A event: 0x%02X", recv_buff[0]);
+
+                switch (recv_buff[0]) {
+                    case 0x11:
+                        LOG_DEBUG("DS4 0x11 event: battery level %d%%", recv_buff[1]);
+                        break;
+
+                    default:
+                        LOG_DEBUG("Unknown DS4 event: 0x%02X", recv_buff[0]);
+                        break;
+                }
+
+                break;
+
+            case 0x0B: /* HID reply to 1-type request */
+                LOG_DEBUG("DS4 0x0B event: 0x%02X", recv_buff[0]);
+                break;
+
+            default:
+                LOG_DEBUG("Unknown event id: 0x%02X", hid_event.id);
+                break;
+        }
+    }
+
+    return 0;
+}
+
+/**
  * Iterate through all paired bluetooth devices and log their information.
  *
  * TODO:
