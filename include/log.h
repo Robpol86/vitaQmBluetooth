d700ce95ec61ce80cd2d5ef57125f15b9cb6af1b
Robpol86 <robpol86@gmail.com>
2026-05-16 13:04:04 +0800

    Log colors (#100)
    * LOG_FORMAT_
    
    * Color red; doesn't work
    
    * works
    
    * LOG_FORMAT_VALUES_
    
    * rename _time
    
    * test script
    
    * No colors on release
    
    * LOG_FORMAT_ORIGIN_
    
    * bold ERROR, cleanup
    
    * fix typo

--- include/log.h
+++ include/log.h
@@ -22,13 +22,30 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 #ifndef LOG_H
 #define LOG_H
 
+#ifndef NDEBUG
+#define LOG_COLOR_RESET_ "\033[0m"
+#define LOG_COLOR_BOLD_ "\033[1m"
+#define LOG_COLOR_RED_ "\033[1;91m"
+#define LOG_COLOR_GRAY_ "\033[90m"
+#else
+#define LOG_COLOR_RESET_ ""
+#define LOG_COLOR_BOLD_ ""
+#define LOG_COLOR_RED_ ""
+#define LOG_COLOR_GRAY_ ""
+#endif  // NDEBUG
+
+#define LOG_FORMAT_TIME_ LOG_COLOR_GRAY_ "[%02d:%02d:%02d.%03d]" LOG_COLOR_RESET_
+#define LOG_FORMAT_ORIGIN_ LOG_COLOR_BOLD_ "%s:%d:%s" LOG_COLOR_RESET_
+#define LOG_FORMAT_(category) LOG_FORMAT_TIME_ " [" category "] [" MODULE_NAME "] [" LOG_FORMAT_ORIGIN_ "] "
+#define LOG_FORMAT_VALUES_ dt_.hour, dt_.minute, dt_.second, dt_.microsecond / 1000, __FILE__, __LINE__, __func__
+
 /**
  * Macro that logs an error message.
  *
- * @param fmtMsg The log message including any format specifiers.
+ * @param msg The log message (supports %s and other format specifiers).
  * @param ... Arguments for the format specifiers.
  */
-#define LOG_ERROR(fmtMsg, ...) LOG_MESSAGE_(0, "ERROR", fmtMsg, ##__VA_ARGS__)
+#define LOG_ERROR(msg, ...) LOG_MESSAGE_(0, LOG_FORMAT_(LOG_COLOR_RED_ "ERROR" LOG_COLOR_RESET_), msg, ##__VA_ARGS__)
 
 #ifndef NDEBUG
 /**
@@ -37,12 +54,12 @@ this program. If not, see <https://www.gnu.org/licenses/>.
  * If log messages overwrite each other try setting the delay to 50000.
  *
  * @param delay Delay the thread for these many microseconds if >0 (mitigates clobbering).
- * @param fmtMsg The log message including any format specifiers.
+ * @param msg The log message (supports %s and other format specifiers).
  * @param ... Arguments for the format specifiers.
  */
-#define LOG_DEBUG(delay, fmtMsg, ...) LOG_MESSAGE_(delay, "DEBUG", fmtMsg, ##__VA_ARGS__)
+#define LOG_DEBUG(delay, msg, ...) LOG_MESSAGE_(delay, LOG_FORMAT_("DEBUG"), msg, ##__VA_ARGS__)
 #else
-#define LOG_DEBUG(delay, fmtMsg, ...) LOG_MESSAGE_NOOP_(delay, fmtMsg, ##__VA_ARGS__)
+#define LOG_DEBUG(delay, msg, ...) LOG_MESSAGE_NOOP_(delay, "", msg, ##__VA_ARGS__)
 #endif  // NDEBUG
 
 #endif  // LOG_H
