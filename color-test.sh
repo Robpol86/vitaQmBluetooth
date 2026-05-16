b15c6f118b664ce4639af56ba96cc2fc500e21da
Robpol86 <robpol86@gmail.com>
2026-05-16 13:25:50 +0900

    test script

---
+++ color-test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "[13:19:53.028] [ERROR] [vitaQmBluetoothKernel] [bluetooth.c:109:kvqmbtGetPairedDevices] Test"
+echo "[13:19:53.028] [\033[31mERROR\033[0m] [vitaQmBluetoothKernel] [bluetooth.c:109:kvqmbtGetPairedDevices] Test"
