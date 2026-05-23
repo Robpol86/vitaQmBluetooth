7cc2ce86eaa8185b8bcf2db556526c4320723ffe
Robpol86 <robpol86@gmail.com>
2026-05-23 17:32:39 +0800

    Todos

--- include/log.h
+++ include/log.h
@@ -27,6 +27,8 @@ this program. If not, see <https://www.gnu.org/licenses/>.
  * - Log file name format: vitaQmBluetooth-YYYYMMDD.log
  * - logfile.h
  * - no colors in logfile
+ * - make fetched-logs
+ * - make fetched-screenshots
  */
 
 #ifndef LOG_H
