763fd2bc565917dc0ed9630f28c0fbb6f5dd9b80
Robpol86 <robpol86@gmail.com>
2026-05-26 15:47:52 -1000

    ignore remote connect requests

--- module_kernel/src/bt_events.c
+++ module_kernel/src/bt_events.c
@@ -174,6 +174,7 @@ typedef enum VqmbtBtEventId {
     VQMBT_BT_EVENT_CONNECT_RESULT = 0x05,
     VQMBT_BT_EVENT_DISCONNECT = 0x06,
     VQMBT_BT_EVENT_ADD_REMOVE_CONNECTING_DEVICE = 0x07,
+    VQMBT_BT_EVENT_CONNECT_REQUESTED = 0x08,
     VQMBT_BT_EVENT_CONNECT_UNPAIRED = 0x09,
     VQMBT_BT_EVENT_UNKNOWN1 = 0x0E,
     VQMBT_BT_EVENT_UNKNOWN2 = 0x10,
@@ -316,10 +317,11 @@ static int kvqmbtEventThread(SceSize args, void* argp) {
 
     // Register callback.
     const unsigned int id_mask =
-        0xFFFFFFFFU & ~((1U << VQMBT_BT_EVENT_INQUIRY_RESULT) | (1U << VQMBT_BT_EVENT_INQUIRY_STOP) |
-                        (1U << VQMBT_BT_EVENT_PAIRING_REQUEST) | (1U << VQMBT_BT_EVENT_CONNECT_UNPAIRED) |
-                        (1U << VQMBT_BT_EVENT_UNKNOWN1) | (1U << VQMBT_BT_EVENT_UNKNOWN2) |
-                        (1U << VQMBT_BT_EVENT_UNKNOWN3));                    // Ignore irrelevant event IDs
+        0xFFFFFFFFU &
+        ~((1U << VQMBT_BT_EVENT_INQUIRY_RESULT) | (1U << VQMBT_BT_EVENT_INQUIRY_STOP) |
+          (1U << VQMBT_BT_EVENT_PAIRING_REQUEST) | (1U << VQMBT_BT_EVENT_CONNECT_UNPAIRED) |
+          (1U << VQMBT_BT_EVENT_UNKNOWN1) | (1U << VQMBT_BT_EVENT_UNKNOWN2) | (1U << VQMBT_BT_EVENT_UNKNOWN3) |
+          (1U << VQMBT_BT_EVENT_CONNECT_REQUESTED));                         // Ignore irrelevant event IDs
     int ret = ksceBtRegisterCallback(uid_callback, 0, id_mask, 0xFFFFFFFF);  // TODO test flags2 0x0
     LOG_DEBUG(0, "ksceBtRegisterCallback returned 0x%08X", ret);
 
