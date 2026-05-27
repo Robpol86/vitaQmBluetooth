7774f334911a1549ea8d8d0b1e005582bd08d9f4
Robpol86 <robpol86@gmail.com>
2026-05-26 14:02:38 -1000

    LOG_MESSAGE_TO_FILE not "private"

--- include/logfile.h
+++ include/logfile.h
@@ -31,9 +31,9 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 #define LOG_LINE_BUFFER 512
 
 #ifndef NDEBUG
-#define LOG_MESSAGE_TO_FILE_(y, m, d, fmt, ...) logfile_write_line(y, m, d, fmt, ##__VA_ARGS__)
+#define LOG_MESSAGE_TO_FILE(y, m, d, fmt, ...) logfile_write_line(y, m, d, fmt, ##__VA_ARGS__)
 #else
-#define LOG_MESSAGE_TO_FILE_(y, m, d, fmt, ...) ((void)0)
+#define LOG_MESSAGE_TO_FILE(y, m, d, fmt, ...) ((void)0)
 #endif  // NDEBUG
 
 #endif  // LOGFILE_H
