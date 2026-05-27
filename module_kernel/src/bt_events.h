6cfee3ce5e0c0a814ba3f4b955dbe541de4f976f
Robpol86 <robpol86@gmail.com>
2026-05-26 20:04:47 -1000

    comment

--- module_kernel/src/bt_events.h
+++ module_kernel/src/bt_events.h
@@ -16,7 +16,7 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 
 /******************************************************************************
  * @file
- * @brief TODO.
+ * @brief Start and stop the bluetooth event handling thread.
  ******************************************************************************/
 
 #ifndef BT_EVENTS_H
