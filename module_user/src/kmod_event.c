f57425f095cf59bd8a23fa39f8b62bffd4b97b85
Robpol86 <robpol86@gmail.com>
2026-05-31 15:35:53 -0700

    log error

--- module_user/src/kmod_event.c
+++ module_user/src/kmod_event.c
@@ -147,9 +147,10 @@ static int event_thread(SceSize args, void* argp) {
 
     // Get event flag.
     uid_event_flag = kvqmbt_get_wrapped_event_flag();
-    LOG_DEBUG(0, "kvqmbt_get_event_flag returned 0x%08X", uid_event_flag);
+    LOG_DEBUG(0, "kvqmbt_get_wrapped_event_flag returned 0x%08X", uid_event_flag);
     if (uid_event_flag < 0) {
-        LOG_ERROR("BUG");  // TODO handle kernel module not loaded
+        // TODO https://github.com/Robpol86/vitaQmBluetooth/issues/108
+        LOG_ERROR("kvqmbt_get_wrapped_event_flag returned error 0x%08X", uid_event_flag);
         return 0;
     }
 
