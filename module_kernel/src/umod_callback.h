6f7da19d4686639942282d211dbdacedf45ef2ee
Robpol86 <robpol86@gmail.com>
2026-05-31 15:30:07 -0700

    todos

--- module_kernel/src/umod_callback.h
+++ module_kernel/src/umod_callback.h
@@ -16,7 +16,7 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 
 /******************************************************************************
  * @file
- * @brief TODO.
+ * @brief Start and stop the user module event emitting thread.
  ******************************************************************************/
 
 #ifndef UMOD_CALLBACK_H
