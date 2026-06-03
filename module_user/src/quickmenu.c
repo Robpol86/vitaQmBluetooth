dbdc5312d0879c41ee93211d539289b4cc2396d6
Robpol86 <robpol86@gmail.com>
2026-06-02 17:18:00 -0700

    Moved heading text out of plane.

--- module_user/src/quickmenu.c
+++ module_user/src/quickmenu.c
@@ -51,8 +51,8 @@ static VqmbtDeviceInfo devices[VQMBT_MAX_DEVICES];  // TODO locking/semaphore?
 
 // Widget IDs (prefixed because they must be unique across all plugins).
 #define ID_SEPARATOR MODULE_NAME "Separator"
-#define ID_PLANE_ROOT MODULE_NAME "PlaneRoot"
 #define ID_SECTION_TITLE MODULE_NAME "SectionTitle"
+#define ID_PLANE_BUTTONS MODULE_NAME "PlaneButtons"
 
 // Button widget IDs.
 static const char* const ID_BUTTONS[VQMBT_MAX_DEVICES] = {
@@ -181,24 +181,24 @@ void quickmenu_start(void) {
     // Add horizontal line separator.
     QuickMenuRebornSeparator(ID_SEPARATOR, SCE_SEPARATOR_HEIGHT);
 
-    // Add the root plane that holds all other items.
-    QuickMenuRebornRegisterWidget(ID_PLANE_ROOT, NULL, plane);
-    QuickMenuRebornSetWidgetSize(ID_PLANE_ROOT, SCE_PLANE_WIDTH, 700, 0, 0);
-    QuickMenuRebornSetWidgetColor(ID_PLANE_ROOT, 1, 1, 1, 0);
-
     // Add section heading text.
-    QuickMenuRebornRegisterWidget(ID_SECTION_TITLE, ID_PLANE_ROOT, text);
-    QuickMenuRebornSetWidgetSize(ID_SECTION_TITLE, SCE_PLANE_WIDTH, 50, 0, 0);
-    QuickMenuRebornSetWidgetPosition(ID_SECTION_TITLE, -206, 312, 0, 0);
+    QuickMenuRebornRegisterWidget(ID_SECTION_TITLE, NULL, text);
+    QuickMenuRebornSetWidgetSize(ID_SECTION_TITLE, 300, 50, 0, 0);
+    QuickMenuRebornSetWidgetPosition(ID_SECTION_TITLE, -206, 0, 0, 0);
     QuickMenuRebornSetWidgetColor(ID_SECTION_TITLE, 1, 1, 1, 1);
     QuickMenuRebornSetWidgetLabel(ID_SECTION_TITLE, "Bluetooth Devices");
+
+    // Add the button plane that holds all the buttons.
+    QuickMenuRebornRegisterWidget(ID_PLANE_BUTTONS, NULL, plane);
+    QuickMenuRebornSetWidgetSize(ID_PLANE_BUTTONS, SCE_PLANE_WIDTH, 650, 0, 0);
+    QuickMenuRebornSetWidgetColor(ID_PLANE_BUTTONS, 1, 1, 1, 0);
 
     // Add device slot buttons.
     for (int idx = 0; idx < VQMBT_MAX_DEVICES; idx++) {
         const char* id = ID_BUTTONS[idx];
-        QuickMenuRebornRegisterWidget(id, ID_PLANE_ROOT, button);
+        QuickMenuRebornRegisterWidget(id, ID_PLANE_BUTTONS, button);
         QuickMenuRebornSetWidgetSize(id, 600, 75, 0, 0);
-        QuickMenuRebornSetWidgetPosition(id, 20, 243 - (idx * 80), 0, 0);
+        QuickMenuRebornSetWidgetPosition(id, 20, 280 - (idx * 80), 0, 0);
         QuickMenuRebornSetWidgetColor(id, 1, 1, 1, 1);
         char label[BUTTON_LABEL_MAX];
         sceClibSnprintf(label, sizeof(label), "Slot %d: no device", idx + 1);
@@ -207,9 +207,9 @@ void quickmenu_start(void) {
     }
 
     // Register handlers.
-    const char* last = ID_BUTTONS[VQMBT_MAX_DEVICES - 1];
-    QuickMenuRebornAssignOnLoadHandler(quickmenu_on_load, last);
-    QuickMenuRebornAssignOnDeleteHandler(quickmenu_on_unload, last);
+    const char* last_widget = ID_BUTTONS[VQMBT_MAX_DEVICES - 1];
+    QuickMenuRebornAssignOnLoadHandler(quickmenu_on_load, last_widget);
+    QuickMenuRebornAssignOnDeleteHandler(quickmenu_on_unload, last_widget);
 }
 
 /**
@@ -220,7 +220,7 @@ void quickmenu_stop(void) {
         const char* id = ID_BUTTONS[idx];
         QuickMenuRebornUnregisterWidget(id);
     }
+    QuickMenuRebornUnregisterWidget(ID_PLANE_BUTTONS);
     QuickMenuRebornUnregisterWidget(ID_SECTION_TITLE);
-    QuickMenuRebornUnregisterWidget(ID_PLANE_ROOT);
     QuickMenuRebornRemoveSeparator(ID_SEPARATOR);
 }
