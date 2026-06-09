362ddaf71d39bf3df39044a007b355510b9d80ca
Robpol86 <robpol86@gmail.com>
2026-06-09 15:36:45 -0700

    moved

--- module_user/src/qm_state.c
+++ module_user/src/qm_state.c
@@ -143,21 +143,6 @@ static void refresh_ui(void) {
 }
 
 /**
- * Transtion to BTNSTATE_SLOT_EMPTY_DISABLED.
- */
-static void bulk_updated_remove_device(bool* changed, const int idx) {
-    QmButton* qm_button = &qm_state.buttons[idx];
-    if (qm_button->btn_state != BTNSTATE_SLOT_EMPTY_DISABLED) {
-        LOG_DEBUG(0, "Setting slot %d as empty", idx + 1);
-        sceClibMemset(&qm_button->device, 0, sizeof(qm_button->device));
-        qm_button->btn_state = BTNSTATE_SLOT_EMPTY_DISABLED;
-        *changed = true;
-        return;
-    }
-    LOG_DEBUG(0, "No-op");
-}
-
-/**
  * Transition to BTNSTATE_BT_OFF_DISABLED.
  */
 static void transition_state_bt_off(bool* changed, const int* idx) {
@@ -177,41 +162,6 @@ static void transition_state_bt_off(bool* changed, const int* idx) {
         return;
     }
     LOG_DEBUG(0, "No-op");
-}
-
-/**
- * TODO.
- */
-static void bulk_updated_add_device(bool* changed, const int idx, const VqmbtDeviceInfo* new_device) {
-    QmButton* qm_button = &qm_state.buttons[idx];
-    VqmbtDeviceInfo* old_device = &qm_button->device;
-    if (new_device->mac0 == old_device->mac0 && new_device->mac1 == old_device->mac1) return;
-
-    LOG_DEBUG(0, "New device in slot %d: \"%s\" (was \"%s\")", idx + 1, new_device->name, old_device->name);
-    sceClibMemcpy(old_device, new_device, sizeof(*new_device));
-
-    switch (old_device->state) {
-        case VQMBT_BT_STATE_DISCONNECTED:
-            qm_button->btn_state = BTNSTATE_DISCONNECTED;
-            break;
-        case VQMBT_BT_STATE_CONNECTING:
-            qm_button->btn_state = BTNSTATE_CONNECTING_DISABLED;
-            break;
-        case VQMBT_BT_STATE_DISCONNECTING:
-            qm_button->btn_state = BTNSTATE_DISCONNECTING_DISABLED;
-            break;
-        case VQMBT_BT_STATE_REGISTERING:
-            LOG_DEBUG(0, "Handling registering state as connected for device \"%s\"", old_device->name);
-        case VQMBT_BT_STATE_CONNECTED:
-            qm_button->btn_state = BTNSTATE_CONNECTED;
-            break;
-        default:
-            LOG_WARN("Unhandled state=%d for device \"%s\"", old_device->state, old_device->name);
-            qm_button->btn_state = BTNSTATE_DISCONNECTED;
-            break;
-    }
-
-    *changed = true;
 }
 
 /**
@@ -338,6 +288,56 @@ static void transition_state_bt_on(bool* changed) {
                 break;
         }
     }
+}
+
+/**
+ * Transtion to BTNSTATE_SLOT_EMPTY_DISABLED.
+ */
+static void bulk_updated_remove_device(bool* changed, const int idx) {
+    QmButton* qm_button = &qm_state.buttons[idx];
+    if (qm_button->btn_state != BTNSTATE_SLOT_EMPTY_DISABLED) {
+        LOG_DEBUG(0, "Setting slot %d as empty", idx + 1);
+        sceClibMemset(&qm_button->device, 0, sizeof(qm_button->device));
+        qm_button->btn_state = BTNSTATE_SLOT_EMPTY_DISABLED;
+        *changed = true;
+        return;
+    }
+    LOG_DEBUG(0, "No-op");
+}
+
+/**
+ * TODO.
+ */
+static void bulk_updated_add_device(bool* changed, const int idx, const VqmbtDeviceInfo* new_device) {
+    QmButton* qm_button = &qm_state.buttons[idx];
+    VqmbtDeviceInfo* old_device = &qm_button->device;
+    if (new_device->mac0 == old_device->mac0 && new_device->mac1 == old_device->mac1) return;
+
+    LOG_DEBUG(0, "New device in slot %d: \"%s\" (was \"%s\")", idx + 1, new_device->name, old_device->name);
+    sceClibMemcpy(old_device, new_device, sizeof(*new_device));
+
+    switch (old_device->state) {
+        case VQMBT_BT_STATE_DISCONNECTED:
+            qm_button->btn_state = BTNSTATE_DISCONNECTED;
+            break;
+        case VQMBT_BT_STATE_CONNECTING:
+            qm_button->btn_state = BTNSTATE_CONNECTING_DISABLED;
+            break;
+        case VQMBT_BT_STATE_DISCONNECTING:
+            qm_button->btn_state = BTNSTATE_DISCONNECTING_DISABLED;
+            break;
+        case VQMBT_BT_STATE_REGISTERING:
+            LOG_DEBUG(0, "Handling registering state as connected for device \"%s\"", old_device->name);
+        case VQMBT_BT_STATE_CONNECTED:
+            qm_button->btn_state = BTNSTATE_CONNECTED;
+            break;
+        default:
+            LOG_WARN("Unhandled state=%d for device \"%s\"", old_device->state, old_device->name);
+            qm_button->btn_state = BTNSTATE_DISCONNECTED;
+            break;
+    }
+
+    *changed = true;
 }
 
 /**
