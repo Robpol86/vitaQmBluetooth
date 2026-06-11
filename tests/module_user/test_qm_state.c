f69c6a1b80eda4df3cd1ea49fd0e6b31f4a1c720
Robpol86 <robpol86@gmail.com>
2026-06-11 15:29:38 -0700

    test_bulk_from_clean_one_device done

--- tests/module_user/test_qm_state.c
+++ tests/module_user/test_qm_state.c
@@ -69,7 +69,31 @@ static void test_bulk_from_clean_no_devices(void** state) {
 static void test_bulk_from_clean_one_device(void** state) {
     (void)state;
 
-    skip();  // static_assert(1 == 1, "TODO");  // TODO
+    // Run.
+    const VqmbtDeviceInfo devices[VQMBT_MAX_DEVICES] = {
+        [0] = {.name = "Test Device", .mac0 = 0x12345678, .mac1 = 0x9ABCDEF0, .state = VQMBT_BT_STATE_DISCONNECTED},
+    };
+    const bool changed = qm_state_update_ui(&(QmsRequest){
+        .id = QMS_REQUEST_BULK_UPDATE,
+        .bulk.bluetooth_on = true,
+        .bulk.num_devices = 1,
+        .bulk.devices = devices,
+    });
+
+    // Verify.
+    assert_true(changed);
+    assert_string_equal(qm_state.buttons[0].device.name, "Test Device");
+    assert_int_equal(qm_state.buttons[0].device.mac0, 0x12345678);
+    assert_int_equal(qm_state.buttons[0].device.mac1, 0x9ABCDEF0);
+    assert_int_equal(qm_state.buttons[0].device.state, VQMBT_BT_STATE_DISCONNECTED);
+    assert_int_equal(qm_state.buttons[0].btn_state, BTNSTATE_DISCONNECTED);
+    for (int i = 1; i < VQMBT_MAX_DEVICES; i++) {
+        assert_string_equal(qm_state.buttons[i].device.name, "");
+        assert_int_equal(qm_state.buttons[i].device.mac0, 0);
+        assert_int_equal(qm_state.buttons[i].device.mac1, 0);
+        assert_int_equal(qm_state.buttons[i].device.state, VQMBT_BT_STATE_UNKNOWN0);
+        assert_int_equal(qm_state.buttons[i].btn_state, BTNSTATE_SLOT_EMPTY_DISABLED);
+    }
 }
 
 static void test_bulk_from_clean_max_devices(void** state) {
