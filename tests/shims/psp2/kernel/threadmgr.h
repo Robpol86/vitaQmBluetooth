e50b205660c55ed56298d2a1f67595a67c8354eb
Robpol86 <robpol86@gmail.com>
2026-06-10 18:12:04 -0700

    wip fixed more

--- tests/shims/psp2/kernel/threadmgr.h
+++ tests/shims/psp2/kernel/threadmgr.h
@@ -1,4 +1,8 @@
 #ifndef SHIM_PSP2_THREADMGR_H
 #define SHIM_PSP2_THREADMGR_H
 static inline int sceKernelGetThreadId(void) { return 0; }
+static inline int sceKernelDelayThread(unsigned int us) {
+    (void)us;
+    return 0;
+}
 #endif
--- tests/shims/psp2/kernel/threadmgr/lw_mutex.h
+++ tests/shims/psp2/kernel/threadmgr/lw_mutex.h
@@ -3,6 +3,20 @@
 typedef struct SceKernelLwMutexWork {
     int _opaque[4];
 } SceKernelLwMutexWork;
+
+static inline int sceKernelCreateLwMutex(SceKernelLwMutexWork* work, const char* name, unsigned int attr, int init_count,
+                                         void* opt) {
+    (void)work;
+    (void)name;
+    (void)attr;
+    (void)init_count;
+    (void)opt;
+    return 0;
+}
+static inline int sceKernelDeleteLwMutex(SceKernelLwMutexWork* work) {
+    (void)work;
+    return 0;
+}
 static inline int sceKernelLockLwMutex(SceKernelLwMutexWork* m, int n, unsigned int* t) {
     (void)m;
     (void)n;
