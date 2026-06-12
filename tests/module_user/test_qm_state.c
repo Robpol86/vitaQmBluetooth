c85e5a7f76ec9c2569aabac8ff201d314ace8199
Robpol86 <robpol86@gmail.com>
2026-06-11 20:09:32 -0700

    BUG FOUND but skipping test for now

--- tests/module_user/test_qm_state.c
+++ tests/module_user/test_qm_state.c
@@ -96,6 +96,37 @@ static void test_bulk_from_clean_one_device(void** state) {
     }
 }
 
+static void test_bulk_from_clean_one_device_bt_off(void** state) {
+    (void)state;
+
+    // Run.
+    const VqmbtDeviceInfo devices[VQMBT_MAX_DEVICES] = {
+        [0] = {.name = "Test Device", .mac0 = 0x12345678, .mac1 = 0x9ABCDEF0, .state = VQMBT_BT_STATE_DISCONNECTED},
+    };
+    const bool changed = qm_state_update_ui(&(QmsRequest){
+        .id = QMS_REQUEST_BULK_UPDATE,
+        .bulk.bluetooth_on = false,
+        .bulk.num_devices = 1,
+        .bulk.devices = devices,
+    });
+
+    // Verify.
+    skip();  // TODO remove
+    assert_true(changed);
+    assert_string_equal(qm_state.buttons[0].device.name, "Test Device");
+    assert_int_equal(qm_state.buttons[0].device.mac0, 0x12345678);
+    assert_int_equal(qm_state.buttons[0].device.mac1, 0x9ABCDEF0);
+    assert_int_equal(qm_state.buttons[0].device.state, VQMBT_BT_STATE_DISCONNECTED);
+    assert_int_equal(qm_state.buttons[0].btn_state, BTNSTATE_BT_OFF_DISABLED);
+    for (int i = 1; i < VQMBT_MAX_DEVICES; i++) {
+        assert_string_equal(qm_state.buttons[i].device.name, "");
+        assert_int_equal(qm_state.buttons[i].device.mac0, 0);
+        assert_int_equal(qm_state.buttons[i].device.mac1, 0);
+        assert_int_equal(qm_state.buttons[i].device.state, VQMBT_BT_STATE_UNKNOWN0);
+        assert_int_equal(qm_state.buttons[i].btn_state, BTNSTATE_SLOT_EMPTY_DISABLED);
+    }
+}
+
 static void test_bulk_from_clean_max_devices(void** state) {
     (void)state;
 
@@ -148,12 +179,6 @@ static void test_bulk_from_clean_max_devices(void** state) {
         assert_int_equal(qm_state.buttons[0].btn_state, BTNSTATE_DISCONNECTED);
     }
     static_assert(sizeof(devices) / sizeof(devices[0]) == VQMBT_MAX_DEVICES, "VQMBT_MAX_DEVICES changed, test outdated");
-}
-
-static void test_bulk_from_clean_one_device_bt_off(void** state) {
-    (void)state;
-
-    skip();  // static_assert(1 == 1, "TODO");  // TODO
 }
 
 static void test_bulk_from_clean_one_device_already_connected(void** state) {
