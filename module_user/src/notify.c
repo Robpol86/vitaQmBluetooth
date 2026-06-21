24811851c673f12a3c07f681ee19865efc2145b2
Robpol86 <robpol86@gmail.com>
2026-06-21 14:46:09 -0500

    Library not found: [SceNotificationUtil, ver=1]

--- module_user/src/notify.c
+++ module_user/src/notify.c
@@ -21,13 +21,45 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 
 #include "notify.h"
 
+#include <psp2/notificationutil.h>
+
 #include "log.h"
+
+/**
+ * TODO remove.
+ */
+static void utf8_to_utf16(const uint8_t* src, uint16_t* dst) {
+    int i;
+    for (i = 0; src[i];) {
+        if ((src[i] & 0xE0) == 0xE0) {
+            *(dst++) = ((src[i] & 0x0F) << 12) | ((src[i + 1] & 0x3F) << 6) | (src[i + 2] & 0x3F);
+            i += 3;
+        } else if ((src[i] & 0xC0) == 0xC0) {
+            *(dst++) = ((src[i] & 0x1F) << 6) | (src[i + 1] & 0x3F);
+            i += 2;
+        } else {
+            *(dst++) = src[i];
+            i += 1;
+        }
+    }
+
+    *dst = '\0';
+}
 
 /**
  * TODO
  */
 void notify_if_debug(void) {
-#ifndef NDEBUG
-    LOG_WARN("TODO");
+#ifdef NDEBUG
+    return;
 #endif
+    char text_buf[SCE_NOTIFICATIONUTIL_TEXT_MAX * 2];
+    sceClibSnprintf(text_buf, sizeof(text_buf), "Hello World");
+
+    SceNotificationUtilProgressInitParam param;
+    sceClibMemset(&param, 0, sizeof(SceNotificationUtilProgressInitParam));
+    utf8_to_utf16((uint8_t*)text_buf, param.notificationText);
+
+    SceInt32 ret = sceNotificationUtilSendNotification(param.notificationText);
+    LOG_DEBUG(0, "sceNotificationUtilSendNotification returned 0x%08X", ret);
 }
