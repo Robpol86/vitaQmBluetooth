2cb163501f5964414aafb444cd79f5b4544ade73
Robpol86 <robpol86@gmail.com>
2026-06-28 11:42:05 -0500

    done with common

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -7,3 +7,31 @@ function(common_forward_module_name_project_name target)
     PROJECT_NAME="${PROJECT_NAME}"
   )
 endfunction()
+
+# Set common include directories.
+function(common_include_directories target)
+  target_include_directories("${target}" PRIVATE
+    "${CMAKE_SOURCE_DIR}/include"
+    "${CMAKE_BINARY_DIR}/cmake_gen"
+  )
+endfunction()
+
+# Set common compile options.
+function(common_compile_options target)
+  target_compile_options("${target}" PRIVATE
+    -Wall
+    -Werror=format-security
+    -Werror=strict-prototypes
+    -Wformat=2
+    -ffile-prefix-map=${CMAKE_CURRENT_SOURCE_DIR}/src/=
+  )
+endfunction()
+
+# Set common link options.
+function(common_link_options target)
+  target_link_options("${target}" PRIVATE
+    -Wl,-e,module_start
+    -Wl,-q
+    -nostdlib
+  )
+endfunction()
