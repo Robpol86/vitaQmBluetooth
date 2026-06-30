942816a27e7e37bfa23137611cdc0db75a35a0a7
Robpol86 <robpol86@gmail.com>
2026-06-29 19:17:47 -0500

    placeholder test file for user module

---
+++ tests/module_user/qm_state_c/test_bulk_update.c
@@ -0,0 +1,49 @@
+/*
+This file is part of vitaQmBluetooth.
+Copyright © 2026 Robpol86
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/******************************************************************************
+ * @file
+ * @brief TODO.
+ ******************************************************************************/
+
+#include <cmocka.h>
+
+// TODO #include "qm_state.c"
+#include "fff.h"
+
+DEFINE_FFF_GLOBALS;
+
+static int setup(void** state) {
+    (void)state;
+
+    // TODO
+
+    return 0;
+}
+
+static void test_bulk_from_clean_one_device(void** state) {
+    (void)state;
+
+    assert_int_equal(1, 1);
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test_setup(test_bulk_from_clean_one_device, setup),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
