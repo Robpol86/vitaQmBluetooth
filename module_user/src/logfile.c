f5746bb97182ee4f2950792e14b766418bf58c11
Robpol86 <robpol86@gmail.com>
2026-07-15 14:52:53 +0200

    cppcoreguidelines-init-variables

--- module_user/src/logfile.c
+++ module_user/src/logfile.c
@@ -34,7 +34,7 @@ static bool is_initialized = false;
  */
 void logfile_init(void) {
 #ifndef NDEBUG
-    int ret;
+    int ret = 0;
 
     // Create log directories.
     ret = sceIoMkdir(LOGFILE_DIR_PARENT, 0777);
@@ -68,7 +68,7 @@ void logfile_write_line(int y, int m, int d, const char* line, ...) {
     if (!is_initialized) {
         return;
     }
-    int ret;
+    int ret = 0;
 
     // Determine filename.
     char log_file_path[256];
